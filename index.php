0d82fb6f80d211035324b268aaf406a4c7868038
Helder Ramos <helder.ramos1347@gmail.com>
2016-08-30 14:05:14 -0300

    Configurando o PHP

--- index.php
+++ index.php
@@ -1,8 +1,29 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        
+        <!-- Define a viewport -->
+        <meta name="viewport" content="width = device-width, initial-scale = 1.0"/>
+        
+        <!-- Adiciona o CSS do Bootstrap -->
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+        <!-- Adiciona o CSS personalizado -->
+        <link href="bootstrap/css/main.css" rel="stylesheet"/>
+        <title>Easy Donation</title>
+    </head>
+    <body>
+        
+        <?php
+        echo 'Testando o php';
+        ?>
+        
+        
+        <!-- Adiciona o Jquery na pagina-->
+        <script src="bootstrap/js/jquery-3.1.0.min.js"></script>
+        <!-- Adiciona o Javascript do Bootstrap -->
+        <script src="bootstrap/js/bootstrap.min.js"> </script>
+        <!-- Adiciona o Javascript personalizado -->
+        <script src="bootstrap/js/main.js"></script>
+    </body>
+</html>
